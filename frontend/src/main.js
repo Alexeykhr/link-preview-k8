2e557e7ca02c2365fb0c37b5fd8bc2b75c4d0f75
Alexey Khrushch <alexeykhr@outlook.com>
2020-03-12 22:17:36 +0200

    chore: add loading

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,26 @@
-if (process.env.NODE_ENV === 'development') {
-  if (module.hot) {
-    module.hot.accept()
-  }
-}
+'use strict'
+
+const input = document.querySelector('#input')
+const loading = document.querySelector('#loading')
+
+const TIMEOUT_MS = 500
+let timeout
+
+input.addEventListener('input', (evt) => {
+  loading.classList.remove('hide')
+
+  clearTimeout(timeout)
+
+  timeout = setTimeout(() => {
+    fetch('/api/website?input=' + decodeURI(evt.target.value))
+      .then(() => {
+
+      })
+      .catch(() => {
+
+      })
+      .finally(() => {
+        setTimeout(() => loading.classList.add('hide'), 250)
+      })
+  }, TIMEOUT_MS)
+})
