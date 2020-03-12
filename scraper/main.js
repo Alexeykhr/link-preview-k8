3ffa3a10524a29f58c7e07c3f2389df94bcb77b4
Alexey Khrushch <alexeykhr@outlook.com>
2020-03-12 22:37:42 +0200

    chore: prepare scraper

---
+++ scraper/main.js
@@ -0,0 +1,22 @@
+// const amqp = require('amqplib/callback_api')
+// const puppeteer = require('puppeteer')
+// const redis = require('redis')
+
+// const client = redis.createClient()
+//
+// client.on('error', function (error) {
+//   console.error(error)
+// })
+
+// amqp.connect('amqp://localhost', (err, conn) => {
+//   console.log(err, conn)
+// })
+
+// ;(async () => {
+//   const browser = await puppeteer.launch()
+//   const page = await browser.newPage()
+//   await page.goto('https://example.com')
+//   await page.screenshot({ path: 'example.png' })
+//
+//   await browser.close()
+// })()
